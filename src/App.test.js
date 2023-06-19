2c144c04dfd13bad33daa3089a27268a514bb198
liannnaa <lnp2016@nyu.edu>
2023-06-19 18:20:18 -0500

    fixed date error with tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, within } from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
@@ -18,56 +18,49 @@ afterEach(() => {
 
 test('test that App component renders', () => {
   render(<App />, container);
- });
+});
 
- test('test that App component renders Task', () => {
+test('test that App component renders Task', () => {
   render(<App />);
-  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i})
-  const element = screen.getByRole('button', {name: /Add/i}) ;
-  fireEvent.change(inputTask, { target: { value: "History Test"}})
-  fireEvent.click(element)
-  // You may need to add delay or use waitFor because your state update and render might be asynchronous
-  // This would be better than relying on the exact date string as it might lead to flaky tests
-  const check = screen.getByText(/History Test/i)
-  expect(check).toBeInTheDocument();
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const addButton = screen.getByRole('button', {name: /Add/i});
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.click(addButton);
+  const task = screen.getByText(/History Test/i);
+  expect(task).toBeInTheDocument();
+  expect(inputTask.value).toBe('');
+});
 
-
- test('test that App component doesn\'t render dupicate Task', () => {
+test('test that App component doesn\'t render duplicate Task', () => {
   render(<App />);
-  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i})
-  const element = screen.getByRole('button', {name: /Add/i}) ;
-  fireEvent.change(inputTask, { target: { value: "History Test"}})
-  fireEvent.click(element)
-  const date = Date().toLocaleString('en-US').split(" ",4).join(' ');
-  const checkDate = screen.getByText(new RegExp(date, "i"))
-  expect(checkDate).toBeInTheDocument();
-  fireEvent.change(inputTask, { target: { value: "History Test"}})
-  fireEvent.click(element)
-  const check = screen.getAllByText(/Test/i)
-  expect(check.length).toBe(1);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const addButton = screen.getByRole('button', {name: /Add/i});
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.click(addButton);
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.click(addButton);
+  const tasks = screen.getAllByText(/History Test/i);
+  expect(tasks.length).toBe(1);
+});
 
- test('test that App component doesn\'t add a blank task', () => {
+test('test that App component doesn\'t add a blank task', () => {
   render(<App />);
-  const element = screen.getByRole('button', {name: /Add/i}) ;
-  fireEvent.click(element)
-  const check = screen.getByText(/You have no todo's left/i)
-  expect(check).toBeInTheDocument();
- });
+  const addButton = screen.getByRole('button', {name: /Add/i});
+  fireEvent.click(addButton);
+  const noTodosText = screen.getByText(/You have no todo's left/i);
+  expect(noTodosText).toBeInTheDocument();
+});
 
- test('test that App component can be deleted thru checkbox', () => {
-    render(<App />);
-    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i})
-    const element = screen.getByRole('button', {name: /Add/i}) ;
-    fireEvent.change(inputTask, { target: { value: "History Test"}})
-    fireEvent.click(element)
-    const date = Date().toLocaleString('en-US').split(" ",4).join(' ');
-    const checkDate = screen.getByText(new RegExp(date, "i"))
-    expect(checkDate).toBeInTheDocument();
-    const checkTask = screen.getByRole('checkbox')
-    fireEvent.click(checkTask)
-    const check = screen.getByText(/You have no todo's left/i)
-    expect(check).toBeInTheDocument();
-   });
-  
+test('test that App component can be deleted thru checkbox', async () => {
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const addButton = screen.getByRole('button', {name: /Add/i});
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.click(addButton);
+  const task = await screen.findByText(/History Test/i);
+  expect(task).toBeInTheDocument();
+  const checkbox = within(task).getByRole('checkbox');
+  fireEvent.click(checkbox);
+  const noTodosText = await screen.findByText(/You have no todo's left/i);
+  expect(noTodosText).toBeInTheDocument();
+});
